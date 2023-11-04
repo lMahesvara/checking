9465737ea674ab3c2f1edbe446c89661e38b21a0
Erick Bernal Amparano <erick082016@gmail.com>
2023-11-03 19:10:14 -0700

    change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' bg-zinc-100'}>{children}</body>
+      <body className={inter.className + ' bg-zinc-900'}>{children}</body>
     </html>
   )
 }
