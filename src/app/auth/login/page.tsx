a02ecbbd90b98d044b88c9a80d9700b9a9d85e6b
Erick Bernal Amparano <erick082016@gmail.com>
2023-11-06 13:27:04 -0700

    feat: create protected routes and redirect to login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from '@/components/Login'
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Login />
+    </main>
+  )
+}
+export default page
