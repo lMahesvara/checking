d22d93576a938f101b62baa72fdcb944d55011ad
Erick Bernal Amparano <erick082016@gmail.com>
2023-11-06 14:23:38 -0700

    remove root page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from '@/components/Login'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Login />
-    </main>
-  )
-}
