e1dce958aef6ca52f2b20553e462773d790ff7d0
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-11-06 23:03:48 -0700

    feat: create sign up component

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignUp from "@/components/sign-up";
+
+const page = () => {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SignUp />
+      </main>
+    )
+  }
+  export default page
