5ef0a1e17f5abd356a07e583d890eca017d78b01
Erick Bernal Amparano <erick082016@gmail.com>
2023-11-09 16:35:58 -0700

    feat: add skeletons

---
+++ src/components/skeletons/UserSkeleton.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from '@nextui-org/skeleton'
+
+const UserSkeleton = () => {
+  return (
+    <div>
+      <Skeleton className="flex rounded-full w-8 h-8" />
+    </div>
+  )
+}
+export default UserSkeleton
